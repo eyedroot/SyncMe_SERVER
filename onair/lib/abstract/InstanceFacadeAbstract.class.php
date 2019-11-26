de0a112a195a8ba822d44628a7c54ef8afab8045
byzz <byzz@inven.co.kr>
2019-11-26 15:17:58 +0900

    InstanceFacade clone 메서드 설명 추가

--- onair/lib/abstract/InstanceFacadeAbstract.class.php
+++ onair/lib/abstract/InstanceFacadeAbstract.class.php
@@ -9,5 +9,17 @@ namespace onair\lib\abstract;
  * 구현 가능한 메서드들을 나열해보고 테스트를 주 목적으로 한다
  */
 abstract class InstanceFacadeAbstract {
+
+    /**
+     * singleton으로 관리되는 모든 클래스의 또 다른 인스턴트가 필요할 때
+     * 사용되는 메서드이다
+     * 
+     * 어떠한 방식으로 static 변수에 담긴 인스턴트를 찾을지
+     * 어떠한 방식으로 새로 생성한 static 변수를 담을지
+     * 조금은 고민이 더 필요한 메서드이다
+     *
+     * @return mixed
+     */
     abstract function clone();
+    
 }
