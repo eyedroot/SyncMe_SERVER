70a38a8b5ec02c06c12894df659f9f090f1331db
byzz <eyedroot@gmail.com>
2019-11-30 18:38:11 +0900

    User 클래스 주석 첨부 추가

--- onair/lib/User.class.php
+++ onair/lib/User.class.php
@@ -5,9 +5,17 @@ namespace onair\lib;
 class User 
 {
 
+    /**
+     * 상태코드
+     * 
+     * 해당 유저가 이미 존재하면 해당 코드를 클라이언트에 전송
+     */
     const CODE_ALREADY_EXISTS_USER = 0x02;
 
-    static $_db_table_user = 'syncme.user';
+    /**
+     * 유저 정보가 담기는 콜렉션 이름
+     */
+    static $_db_collection = 'syncme.user';
 
     /**
      * 해당 이메일로 유저가 가입되어 있는지 확인한다
@@ -19,7 +27,7 @@ class User
         $db = handleDB('mongo');
 
         $query = new \MongoDB\Driver\Query(['email' => $email]);
-        $rows = $db->executeQuery(static::$_db_table_user, $query)->toArray();
+        $rows = $db->executeQuery(static::$_db_collection, $query)->toArray();
 
         return !! (
             (count($rows) != 0) ? true : false
